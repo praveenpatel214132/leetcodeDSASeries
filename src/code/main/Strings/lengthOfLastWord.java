1de86dde1875b6fa1d389a513a94bd1ac2e4c752
praveenpatel214132 <praveenpatel2141@gmail.com>
2025-07-23 00:02:20 +0530

    All Test cases passed -- lengthOfLastWord

---
+++ src/code/main/Strings/lengthOfLastWord.java
@@ -0,0 +1,9 @@
+package code.main.Strings;
+
+public class lengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        String[] arr = s.split(" ");
+        int k = arr[arr.length - 1].length();
+        return k;
+    }
+}

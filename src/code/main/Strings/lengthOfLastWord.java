b3982d613e89b0446d835cd7c8ca641ec899c805
praveenpatel214132 <praveenpatel2141@gmail.com>
2025-07-23 00:09:29 +0530

    new commit

--- src/code/main/Strings/lengthOfLastWord.java
+++ src/code/main/Strings/lengthOfLastWord.java
@@ -2,6 +2,7 @@ package code.main.Strings;
 
 public class lengthOfLastWord {
     public int lengthOfLastWord(String s) {
+
         String[] arr = s.split(" ");
         int k = arr[arr.length - 1].length();
         return k;

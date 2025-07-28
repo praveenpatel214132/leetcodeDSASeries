ca5b13870c3ea47fa1b187b1bfab9bbbaa870f2d
praveenpatel214132 <praveenpatel2141@gmail.com>
2025-07-28 23:12:32 +0530

    All Test Cases Passed.

---
+++ src/code/main/Arrays/SingleNumber.java
@@ -0,0 +1,11 @@
+package code.main.Arrays;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i:nums){
+            ans^=i;
+        }
+        return ans;
+    }
+}

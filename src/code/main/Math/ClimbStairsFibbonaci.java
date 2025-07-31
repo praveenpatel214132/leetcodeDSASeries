32ca0f8efc41b6d063de8a9719d2eebe2317c98a
praveenpatel214132 <praveenpatel2141@gmail.com>
2025-07-31 23:59:57 +0530

    IntToRoman.java Approach 2 Enhanced All Test Cases Passed. Array errors removed

---
+++ src/code/main/Math/ClimbStairsFibbonaci.java
@@ -0,0 +1,16 @@
+package code.main.Math;
+
+public class ClimbStairsFibbonaci {
+    public int climbStairs(int n) {
+        if(n<=3) return n;
+        int f1=2;
+        int f2=3;
+        int a=0;
+        for(int i=3; i<n;i++){
+            a=f1+f2;
+            f1=f2;
+            f2=a;
+        }
+        return a;
+    }
+}
